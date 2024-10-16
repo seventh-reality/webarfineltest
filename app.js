6c184567ac850f87ad4fe2cc80a1d90708ac1c81
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-10-16 11:31:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -174,11 +174,11 @@ class OxExperience {
                 const newDistance = this.getDistance(event.touches);
                 const scale = newDistance / this._lastPinchDistance;
                 this._lastPinchDistance = newDistance;
-                this.scaleCar(this._scene.scale.x * scale);
+                this.scaleCar(this._model.scale.x * scale);
             } else if (event.touches.length === 1 && this._lastTouchX !== null) {
                 const deltaX = event.touches[0].clientX - this._lastTouchX;
                 this._lastTouchX = event.touches[0].clientX;
-                this.rotateCar(this._scene.rotation.y + deltaX * 0.01);
+                this.rotateCar(this._model.rotation.y + deltaX * 0.01);
             }
         }, { passive: true });
 
